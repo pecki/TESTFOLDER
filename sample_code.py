08781b532e6b09038c0660060a529bb92e81854e
Bella Peck <pecki@rpi.edu>
2017-06-27 11:45:26 -0400

    Sum of first 100 integers

--- sample_code.py
+++ sample_code.py
@@ -16,3 +16,12 @@ for x in d:
     s += x
     
 print(s)
+
+# Find the sum of the integers from 1-100:
+    
+sum100 = 0
+for i in list(range(1, 101)):
+    print(i)
+    sum100 += i
+    
+print(sum100)
