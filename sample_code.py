514b1126932db376b739270489dfcb1e9c0b3597
Bella Peck <pecki@rpi.edu>
2017-06-27 11:37:30 -0400

    This is a test to commit sample_code.py to TESTFOLDER. Edited in Spyder.

---
+++ sample_code.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 14 13:14:43 2017
+
+@author: pecki
+"""
+a = ['it']
+b = ['was']
+c = ['bad']
+d = a + b + c
+print(d)
+
+
+s = ''
+for x in d:
+    s += x
+    
+print(s)
